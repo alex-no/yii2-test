e49aff612bedee2fdae957ff46b1e8d083817334
alex-no <alex@4n.com.ua>
2025-04-13 11:03:18 +0300

    Preparing

---
+++ www_app/config/aliases.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    '@app' => dirname(__DIR__),
+    '@web' => '/',
+    '@webroot' => dirname(__DIR__) . '/web',
+    '@apiroot' => dirname(__DIR__) . '/api',
+    '@console' => dirname(__DIR__) . '/console',
+    '@runtime' => dirname(__DIR__) . '/runtime',
+    '@vendor' => dirname(__DIR__) . '/vendor',
+    '@bower' => dirname(__DIR__) . '/vendor/bower-asset',
+    '@npm' => dirname(__DIR__) . '/vendor/npm-asset',
+];
--- www_app/config/console.php
+++ www_app/config/console.php
@@ -1,12 +1,26 @@
 <?php
+$alias = require __DIR__ . '/aliases.php';
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
 
 return yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/common.php',
     [
         'id' => 'console-app',
         'controllerNamespace' => 'console\controllers',
+        'basePath' => dirname(__DIR__),
+        'bootstrap' => ['log'],
         'components' => [
-            // можно подключить лог, базу и т.д.
+            'log' => [
+                'targets' => [
+                    [
+                        'class' => 'yii\log\FileTarget',
+                        'levels' => ['error', 'warning'],
+                    ],
+                ],
+            ],
+            'db' => $db,
         ],
+        'params' => $params,
     ]
 );
