436f529d91b1a8122b7ede44ef0159d3985775d9
alex-no <alex@4n.com.ua>
2025-05-30 20:55:25 +0300

    Move Features List

--- www_app/web/config/route.php
+++ www_app/web/config/route.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    // vue routes
+    'html/<path:.+>' => 'html/index',
+    'html' => 'html/index',
+
+    // api routes
     '' => 'site/index',
     '<controller:\w+>/<id:\d+>' => '<controller>/view',
     '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
