e141285a64ff2d256fda6e29d655fae04b4ecd76
alex-no <alex@4n.com.ua>
2025-05-31 19:16:34 +0300

    Style for login

--- www_front/src/pages/Login.vue
+++ www_front/src/pages/Login.vue
@@ -1,18 +1,22 @@
 <template>
   <form @submit.prevent="handleLogin" class="max-w-md mx-auto bg-white p-6 mt-3 rounded-xl shadow-md">
-    <h2 class="text-xl font-bold mb-4">Login</h2>
+    <h2 class="text-xl font-bold mb-4">{{ $t('form.login') }}</h2>
     <div class="mb-4">
-      <label class="block text-sm font-medium text-gray-700 mb-1">Username</label>
+      <label class="block text-sm text-end font-medium text-gray-700 mb-2" style="width: 140px; margin-right: 10px;">{{ $t('form.username') }}</label>
       <input v-model="username" type="text" class="w-full border border-gray-300 p-2 rounded-md" required />
     </div>
 
     <div class="mb-4">
-      <label class="block text-sm font-medium text-gray-700 mb-1">Password</label>
+      <label class="block text-sm text-end font-medium text-gray-700 mb-2" style="width: 140px; margin-right: 10px;">{{ $t('form.password') }}</label>
       <input v-model="password" type="password" class="w-full border border-gray-300 p-2 rounded-md" required />
     </div>
 
-    <button type="submit" class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md w-full">
-      Sign In
+    <button
+      type="submit"
+      class="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md w-full border border-green-700"
+      style="background-color: #16a34a;"
+    >
+      {{ $t('form.sign_in') }}
     </button>
   </form>
 </template>
