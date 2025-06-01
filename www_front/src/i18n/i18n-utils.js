9bd018f696067ca7dfb6c5827388c44ac8754fe9
alex-no <alex@4n.com.ua>
2025-06-01 10:32:57 +0300

    i18n-utils.js

--- www_front/src/i18n/i18n-utils.js
+++ www_front/src/i18n/i18n-utils.js
@@ -1,14 +1,33 @@
 import { watch } from 'vue'
 
-export function applyI18nEffects(i18n) {
+export function applyI18nEffects(i18n, router) {
+  // Set HTML language
   document.documentElement.lang = i18n.global.locale.value
-  document.title = i18n.global.t('pageTitle')
 
+  // Function to update title based on route
+  const updateTitle = () => {
+    const titleKey = router.currentRoute.value.meta.title
+    if (titleKey) {
+      document.title = i18n.global.t(titleKey)
+    } else {
+      document.title = i18n.global.t('htmlTitle') // fallback
+    }
+  }
+
+  // Watch for language changes
   watch(
     () => i18n.global.locale.value,
     (newLocale) => {
       document.documentElement.lang = newLocale
-      document.title = i18n.global.t('pageTitle')
+      updateTitle()
     }
   )
+
+  // Watch for route changes
+  router.afterEach(() => {
+    updateTitle()
+  })
+
+  // Initialize on load
+  updateTitle()
 }
